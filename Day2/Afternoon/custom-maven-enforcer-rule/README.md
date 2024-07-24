b78b4ba3cfc4d3e3b0cf5ad46419c5ed8266a7f1
Ioannis Kostaras <ikost@apache.org>
2024-07-24 15:15:15 +0300

    Revert "Initial commit of custom enforcer rule to verify existence of artifacts and their content"

--- Day2/Afternoon/custom-maven-enforcer-rule/README.md
+++
@@ -1,3 +0,0 @@
-integration-tests are not executed by default.
-To do so run `mvn verify -Prun-its`
-
--- Day2/Afternoon/custom-maven-enforcer-rule/src/it/it001/src/main/java/com/foo/bar/Main.java
+++
@@ -1,7 +0,0 @@
-package com.foo.bar;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}

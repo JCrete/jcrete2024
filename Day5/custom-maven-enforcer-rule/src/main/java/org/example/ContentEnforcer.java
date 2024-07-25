1cb8ec0578448f7f2be1de07ceeacd295444fa6c
Ignasi Marimon-Clos <ignasi35@gmail.com>
2024-07-25 18:16:03 +0300

    Small fixes on Custom Enforcer Rule

--- Day5/custom-maven-enforcer-rule/src/main/java/org/example/ContentEnforcer.java
+++ Day5/custom-maven-enforcer-rule/src/main/java/org/example/ContentEnforcer.java
@@ -1,21 +1,15 @@
 package org.example;
 
-import javax.inject.Inject;
-import javax.inject.Named;
-
 import org.apache.maven.artifact.Artifact;
 import org.apache.maven.enforcer.rule.api.AbstractEnforcerRule;
 import org.apache.maven.enforcer.rule.api.EnforcerRuleException;
 import org.apache.maven.project.MavenProject;
 
-import java.io.File;
-import java.io.IOException;
+import javax.inject.Inject;
+import javax.inject.Named;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
-import java.util.zip.ZipFile;
 
 @Named("checkWithABadName")
 public class ContentEnforcer extends AbstractEnforcerRule {
@@ -32,25 +26,25 @@ public class ContentEnforcer extends AbstractEnforcerRule {
     @Override
     public void execute() throws EnforcerRuleException {
         Path artifactPath = null;
-        if(classifier == null) {
+        var classifierName = classifier;
+        if (classifier == null) {
+            classifierName = "main";
             artifactPath = project.getArtifact().getFile().toPath();
-        }
-        else {
-            for(Artifact artifact: project.getAttachedArtifacts()) {
-                if(classifier.equals(artifact.getClassifier())) {
+        } else {
+            for (Artifact artifact : project.getAttachedArtifacts()) {
+                if (classifier.equals(artifact.getClassifier())) {
                     artifactPath = artifact.getFile().toPath();
                 }
             }
         }
-        if(artifactPath == null) {
-            throw new EnforcerRuleException("can't find artifact with classifier: " + classifier );
-        }
 
-        if(!Files.exists(artifactPath)) {
-            throw new EnforcerRuleException("Project is missing main artifact");
+        if (artifactPath == null) {
+            throw new EnforcerRuleException("Can't find artifact with classifier: " + classifier);
         }
-        else if(Files.isDirectory(artifactPath)) {
-            throw new EnforcerRuleException("Main artifact is nog a regular file");
+        if (!Files.exists(artifactPath)) {
+            throw new EnforcerRuleException("Project is missing the file for the " + classifierName + " artifact");
+        } else if (Files.isDirectory(artifactPath)) {
+            throw new EnforcerRuleException("The file descriptor " + artifactPath.toString() + " of the artifact " + classifierName + " is not a regular file");
         }
 
         // loop over resources and verify if filesInJar exist AND filesNotInJar don't exist
@@ -58,7 +52,6 @@ public class ContentEnforcer extends AbstractEnforcerRule {
         // for writing tests, have a look at https://github.com/mojohaus/extra-enforcer-rules/tree/master/src/it
         // https://github.com/mojohaus/extra-enforcer-rules/blob/master/pom.xml#L158-L200 shows how the integration-profile is configured
         // you can run this as mvn verify -Prun-its
-
 
     }
 }
